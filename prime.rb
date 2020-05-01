abd288e9b91833a22d1cd406a2a1b4cf5fc8053a
Matt Im <matthew.seiim@gmail.com>
2020-05-01 20:25:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,3 @@
 def prime(number)
-  i=0
-  prime_numbers = []
-  while i < number.length
-    prime_numbers << yield(numbers[i])
-  end
-  if number.prime?
-    true
-  else
-    false
-  end
+  
 end
