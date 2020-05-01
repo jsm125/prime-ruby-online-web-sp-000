edae201f26cbce487ce11d34de6bd817a72d0196
Matt Im <matthew.seiim@gmail.com>
2020-05-01 20:33:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?number)
   if number > 1
-  range = (2..number-1).to_a
-  range.none? do |number-1|
-    number % number-1 == 0
-  
+    range = (2..number-1).to_a
+    range.none? do |number-1|
+      number % number-1 == 0
+    
 end
